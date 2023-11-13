3f3478bd302120785689ff8f4289d2173f886ee0
Pedro Alonso <pedro.alonsodeoliveira@hotmail.com>
2023-11-13 17:20:23 -0300

    base lib added

---
+++ functions.c
@@ -0,0 +1,51 @@
+void escreverJogador() {
+
+}
+
+void lerJogador() {
+
+}
+
+void listarJogadoresAlfabetica() {
+
+}
+
+void listarJogadoresRanking() {
+
+}
+
+void listarJogadoresVitorias() {
+
+}
+
+void alterarJogador() {
+
+}
+
+void inserirResultado() {
+
+}
+
+void listarClassificacao() {
+
+}
+
+void pontuacaoMaiorQue() {
+
+}
+
+void pontuacaoMenorQue() {
+
+}
+
+void buscarJogadorNome() {
+
+}
+
+void buscarJogadorRanking() {
+
+}
+
+void mediaDeSeguidores() {
+
+}
---
+++ functions.h
@@ -0,0 +1,20 @@
+#ifndef FUNCTIONS_
+#define FUNCTIONS_
+
+void escreverJogador();
+void lerJogador();
+void listarJogadoresAlfabetica();
+void listarJogadoresRanking();
+void listarJogadoresVitorias();
+void alterarJogador();
+void inserirResultado();
+void listarClassificacao();
+void pontuacaoMaiorQue();
+void pontuacaoMenorQue();
+void buscarJogadorNome();
+void buscarJogadorRanking();
+void mediaDeSeguidores();
+
+#include "functions.c"
+
+#endif // FUNCTIONS_
